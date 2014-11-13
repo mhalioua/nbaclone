c99e43acca1ec33dc2eb4df200c28f3837359258
aecovarru <aecovarru@gmail.com>
2014-11-13 03:24:57 -0800

    Add

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -106,57 +106,210 @@ class StatisticsController < ApplicationController
  		@guard_game = Array.new
 	 	@guard_matchups.each do |matchup|
 	 		@id = 0
-	 		matchup.player_matchup_game.each do |game|
-	 			# if @id == 0
-	 			# 	@id = game.player_matchup_id
-	 			# 	@MP = 0
-			  #     	@FG = 0
-			  #     	@FGA = 0
-			  #     	@FGP = 0
-			  #     	@ThP = 0
-			  #     	@ThPA = 0
-			  #     	@ThPP = 0
-			  #     	@FT = 0
-			  #     	@FTA = 0
-			  #     	@FTP = 0
-			  #     	@ORB = 0
-			  #     	@DRB = 0
-			  #     	@AST = 0
-			  #     	@STL = 0
-			  #     	@BLK = 0
-			  #     	@TO = 0
-			  #     	@PF = 0
-			  #     	@PTS = 0
-	 			# else
-	 			# 	@player_matchup = game.player_matchup_id
-			  #     	@name = game.name
-			  #     	@date = "Total"
-			  #     	@MP = @MP + game.MP
-			  #     	@FG = @FG + game.FG
-			  #     	@FGA = @FGA + game.FGA
-			  #     	@FGP = @FGP + game.FGP
-			  #     	@ThP = @ThP + game.player_matchup.ThP
-			  #     	@ThPA = @ThPA + game.ThPA
-			  #     	@ThPP = @ThPP + game.ThPP
-			  #     	@FT = @FT + game.ThPP
-			  #     	@FTA = @FTA + game.FTA
-			  #     	@FTP = @FTP + game.FTP
-			  #     	@ORB = @ORB + game.ORB
-			  #     	@DRB = @DRB + game.DRB
-			  #     	@AST = @AST + game.AST
-			  #     	@STL = @STL + game.STL
-			  #     	@BLK = @BLK + game.BLK
-			  #     	@TO = @TO + game.TO
-			  #     	@PF = @PF + game.PF
-			  #     	@PTS = @PTS + game.PTS
-	 			# end
-	 			@guard_game << game
+	 		matchup.player_matchup_game.each_with_index do |game, index|
+	 			if @id == 0
+	 				@id = game.player_matchup_id
+			      	@FG = 0
+			      	@FGA = 0
+			      	@FGP = 0
+			      	@ThP = 0
+			      	@ThPA = 0
+			      	@ThPP = 0
+			      	@FT = 0
+			      	@FTA = 0
+			      	@FTP = 0
+			      	@ORB = 0
+			      	@DRB = 0
+			      	@AST = 0
+			      	@STL = 0
+			      	@BLK = 0
+			      	@TO = 0
+			      	@PF = 0
+			      	@PTS = 0
+			      	@FG_2 = 0
+			      	@FGA_2 = 0
+			      	@FGP_2 = 0
+			      	@ThP_2 = 0
+			      	@ThPA_2 = 0
+			      	@ThPP_2 = 0
+			      	@FT_2 = 0
+			      	@FTA_2 = 0
+			      	@FTP_2 = 0
+			      	@ORB_2 = 0
+			      	@DRB_2 = 0
+			      	@AST_2 = 0
+			      	@STL_2 = 0
+			      	@BLK_2 = 0
+			      	@TO_2 = 0
+			      	@PF_2 = 0
+			      	@PTS_2 = 0
+			    end
+			    if index%2 == 0
+			      	@id = game.player_matchup_id
+			      	@name = game.name
+			      	@MP = "Total"
+			      	@FG = @FG + game.FG
+			      	@FGA = @FGA + game.FGA
+			      	@FGP = @FGP + game.FGP
+			      	@ThP = @ThP + game.ThP
+			      	@ThPA = @ThPA + game.ThPA
+			      	@ThPP = @ThPP + game.ThPP
+			      	@FT = @FT + game.ThPP
+			      	@FTA = @FTA + game.FTA
+			      	@FTP = @FTP + game.FTP
+			      	@ORB = @ORB + game.ORB
+			      	@DRB = @DRB + game.DRB
+			      	@AST = @AST + game.AST
+			      	@STL = @STL + game.STL
+			      	@BLK = @BLK + game.BLK
+			      	@TO = @TO + game.TO
+			      	@PF = @PF + game.PF
+			      	@PTS = @PTS + game.PTS
+			    else
+			    	@id_2 = game.player_matchup_id
+			      	@name_2 = game.name
+			      	@MP_2 = "Total"
+			      	@FG_2 = @FG_2 + game.FG
+			      	@FGA_2 = @FGA_2 + game.FGA
+			      	@FGP_2 = @FGP_2 + game.FGP
+			      	@ThP_2 = @ThP_2 + game.ThP
+			      	@ThPA_2 = @ThPA_2 + game.ThPA
+			      	@ThPP_2 = @ThPP_2 + game.ThPP
+			      	@FT_2 = @FT_2 + game.ThPP
+			      	@FTA_2 = @FTA_2 + game.FTA
+			      	@FTP_2 = @FTP_2 + game.FTP
+			      	@ORB_2 = @ORB_2 + game.ORB
+			      	@DRB_2 = @DRB_2 + game.DRB
+			      	@AST_2 = @AST_2 + game.AST
+			      	@STL_2 = @STL_2 + game.STL
+			      	@BLK_2 = @BLK_2 + game.BLK
+			      	@TO_2 = @TO_2 + game.TO
+			      	@PF_2 = @PF_2 + game.PF
+			      	@PTS_2 = @PTS_2 + game.PTS
+			    end
+			    @guard_game << game
+			end
+	 		@size = (matchup.player_matchup_game.size)/2
+	 		@FGP = (@FGP/@size).round(1)
+	 		@ThPP = (@ThPP/@size).round(1)
+	 		@FTP = (@FTP/@size).round(1)
+	 		@FGP_2 = (@FGP_2/@size).round(1)
+	 		@ThPP_2 = (@ThPP_2/@size).round(1)
+	 		@FTP_2 = (@FTP_2/@size).round(1)
+	 		@date = "Previous " + @size.to_s + " Games"
+	 		if @size != 0 && @size != 1
+		 		@total = PlayerMatchupGame.new(:player_matchup_id => @id, :date => @date, :name => @name, :MP => @MP, :FG => @FG, :FGA => @FGA, :FGP => @FGP, :ThP => @ThP,
+		 			:ThPA => @ThPA, :ThPP => @ThPP, :FT => @FT, :FTA => @FTA, :FTP => @FTP, :ORB => @ORB, :DRB => @DRB, :AST => @AST,
+		 			:STL => @STL, :BLK => @BLK, :TO => @TO, :PF => @PF, :PTS => @PTS)
+		 		@total_2 = PlayerMatchupGame.new(:player_matchup_id => @id_2, :date => @date, :name => @name_2, :MP => @MP_2, :FG => @FG_2, :FGA => @FGA_2, :FGP => @FGP_2, :ThP => @ThP_2,
+		 			:ThPA => @ThPA_2, :ThPP => @ThPP_2, :FT => @FT_2, :FTA => @FTA_2, :FTP => @FTP_2, :ORB => @ORB_2, :DRB => @DRB_2, :AST => @AST_2,
+		 			:STL => @STL_2, :BLK => @BLK_2, :TO => @TO_2, :PF => @PF_2, :PTS => @PTS_2)
+		 		@guard_game << @total
+		 		@guard_game << @total_2
 	 		end
-	 		# PlayerMatchupGame.new(:player_matchup_id => @id, :MP => @MP, :FG => @FG, :FGA => @FGA, :FGP => @FGP, )
 	 	end
 	 	@forward_matchups.each do |matchup|
-	 		matchup.player_matchup_game.each do |game|
+	 		@id = 0
+	 		matchup.player_matchup_game.each_with_index do |game, index|
+	 			if @id == 0
+	 				@id = game.player_matchup_id
+			      	@FG = 0
+			      	@FGA = 0
+			      	@FGP = 0
+			      	@ThP = 0
+			      	@ThPA = 0
+			      	@ThPP = 0
+			      	@FT = 0
+			      	@FTA = 0
+			      	@FTP = 0
+			      	@ORB = 0
+			      	@DRB = 0
+			      	@AST = 0
+			      	@STL = 0
+			      	@BLK = 0
+			      	@TO = 0
+			      	@PF = 0
+			      	@PTS = 0
+			      	@FG_2 = 0
+			      	@FGA_2 = 0
+			      	@FGP_2 = 0
+			      	@ThP_2 = 0
+			      	@ThPA_2 = 0
+			      	@ThPP_2 = 0
+			      	@FT_2 = 0
+			      	@FTA_2 = 0
+			      	@FTP_2 = 0
+			      	@ORB_2 = 0
+			      	@DRB_2 = 0
+			      	@AST_2 = 0
+			      	@STL_2 = 0
+			      	@BLK_2 = 0
+			      	@TO_2 = 0
+			      	@PF_2 = 0
+			      	@PTS_2 = 0
+			    end
+			    if index%2 == 0
+			      	@id = game.player_matchup_id
+			      	@name = game.name
+			      	@MP = "Total"
+			      	@FG = @FG + game.FG
+			      	@FGA = @FGA + game.FGA
+			      	@FGP = @FGP + game.FGP
+			      	@ThP = @ThP + game.ThP
+			      	@ThPA = @ThPA + game.ThPA
+			      	@ThPP = @ThPP + game.ThPP
+			      	@FT = @FT + game.ThPP
+			      	@FTA = @FTA + game.FTA
+			      	@FTP = @FTP + game.FTP
+			      	@ORB = @ORB + game.ORB
+			      	@DRB = @DRB + game.DRB
+			      	@AST = @AST + game.AST
+			      	@STL = @STL + game.STL
+			      	@BLK = @BLK + game.BLK
+			      	@TO = @TO + game.TO
+			      	@PF = @PF + game.PF
+			      	@PTS = @PTS + game.PTS
+			    else
+			    	@id_2 = game.player_matchup_id
+			      	@name_2 = game.name
+			      	@MP_2 = "Total"
+			      	@FG_2 = @FG_2 + game.FG
+			      	@FGA_2 = @FGA_2 + game.FGA
+			      	@FGP_2 = @FGP_2 + game.FGP
+			      	@ThP_2 = @ThP_2 + game.ThP
+			      	@ThPA_2 = @ThPA_2 + game.ThPA
+			      	@ThPP_2 = @ThPP_2 + game.ThPP
+			      	@FT_2 = @FT_2 + game.ThPP
+			      	@FTA_2 = @FTA_2 + game.FTA
+			      	@FTP_2 = @FTP_2 + game.FTP
+			      	@ORB_2 = @ORB_2 + game.ORB
+			      	@DRB_2 = @DRB_2 + game.DRB
+			      	@AST_2 = @AST_2 + game.AST
+			      	@STL_2 = @STL_2 + game.STL
+			      	@BLK_2 = @BLK_2 + game.BLK
+			      	@TO_2 = @TO_2 + game.TO
+			      	@PF_2 = @PF_2 + game.PF
+			      	@PTS_2 = @PTS_2 + game.PTS
+			    end
 	 			@forward_game << game
+	 		end
+	 		@size = (matchup.player_matchup_game.size)/2
+	 		@FGP = (@FGP/@size).round(1)
+	 		@ThPP = (@ThPP/@size).round(1)
+	 		@FTP = (@FTP/@size).round(1)
+	 		@FGP_2 = (@FGP_2/@size).round(1)
+	 		@ThPP_2 = (@ThPP_2/@size).round(1)
+	 		@FTP_2 = (@FTP_2/@size).round(1)
+	 		@date = "Previous " + @size.to_s + " Games"
+	 		if @size != 0 && @size != 1
+		 		@total = PlayerMatchupGame.new(:player_matchup_id => @id, :date => @date, :name => @name, :MP => @MP, :FG => @FG, :FGA => @FGA, :FGP => @FGP, :ThP => @ThP,
+		 			:ThPA => @ThPA, :ThPP => @ThPP, :FT => @FT, :FTA => @FTA, :FTP => @FTP, :ORB => @ORB, :DRB => @DRB, :AST => @AST,
+		 			:STL => @STL, :BLK => @BLK, :TO => @TO, :PF => @PF, :PTS => @PTS)
+		 		@total_2 = PlayerMatchupGame.new(:player_matchup_id => @id_2, :date => @date, :name => @name_2, :MP => @MP_2, :FG => @FG_2, :FGA => @FGA_2, :FGP => @FGP_2, :ThP => @ThP_2,
+		 			:ThPA => @ThPA_2, :ThPP => @ThPP_2, :FT => @FT_2, :FTA => @FTA_2, :FTP => @FTP_2, :ORB => @ORB_2, :DRB => @DRB_2, :AST => @AST_2,
+		 			:STL => @STL_2, :BLK => @BLK_2, :TO => @TO_2, :PF => @PF_2, :PTS => @PTS_2)
+		 		@forward_game << @total
+		 		@forward_game << @total_2
 	 		end
 	 	end
  	end
