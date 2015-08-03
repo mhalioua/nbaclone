9ddfdac29f11c912bb146c0b26c2f7c9c46de896
Mike Halioua <halicracker@yahoo.com>
2015-08-03 16:04:05 -0700

    fix

--- db/migrate/20150803211925_add_column_to_team_data.rb
+++ db/migrate/20150803211925_add_column_to_team_data.rb
@@ -2,6 +2,6 @@ class AddColumnToTeamData < ActiveRecord::Migration
   def change
   	add_column :game_dates, :standard_deviation, :float
   	add_column :game_dates, :mean, :float
-  	add_column :game_dates, :day, :string
+  	add_column :game_dates, :weekday, :string
   end
 end
